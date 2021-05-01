7ed7244ada9b7dc4da4f86e8fec8408e2c956c64
maxminoS <maxminos.1@gmail.com>
2021-05-01 11:16:07 +0700

    Add Touchable component to trigger onPress

--- client/App.tsx
+++ client/App.tsx
@@ -1,66 +1,26 @@
 import React from "react";
+
 import {
 	SafeAreaView,
 	StyleSheet,
-	ScrollView,
 	View,
 	Text,
-	StatusBar,
+	TouchableHighlight
 } from "react-native";
 
-import {
-	Header,
-	LearnMoreLinks,
-	Colors,
-	DebugInstructions,
-	ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-
-declare const global: { HermesInternal: null | {} };
-
 const App = () => {
+	const handlePress = () => {
+		console.log("Pressed!")
+	}
+
 	return (
 		<>
-			<StatusBar barStyle="dark-content" />
-			<SafeAreaView>
-				<ScrollView
-					contentInsetAdjustmentBehavior="automatic"
-					style={styles.scrollView}>
-					<Header />
-					{global.HermesInternal == null ? null : (
-						<View style={styles.engine}>
-							<Text style={styles.footer}>Engine: Hermes</Text>
-						</View>
-					)}
-					<View style={styles.body}>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Step One</Text>
-							<Text style={styles.sectionDescription}>
-								Don't edit <Text style={styles.highlight}>App.tsx</Text> to change this
-								screen and then come back to see your edits.
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>See Your Changes</Text>
-							<Text style={styles.sectionDescription}>
-								<ReloadInstructions />
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Debug</Text>
-							<Text style={styles.sectionDescription}>
-								<DebugInstructions />
-							</Text>
-						</View>
-						<View style={styles.sectionContainer}>
-							<Text style={styles.sectionTitle}>Learn More</Text>
-							<Text style={styles.sectionDescription}>
-								Read the docs to discover what to do next:
-							</Text>
-						</View>
-						<LearnMoreLinks />
-					</View>
-				</ScrollView>
+			<SafeAreaView style={styles.scrollView}>
+				<View style={styles.center}>
+					<TouchableHighlight onPress={handlePress} underlayColor="#7A4988" style={styles.button}>
+						<Text style={styles.buttonText}>Tap me!</Text>
+					</TouchableHighlight>
+				</View>
 			</SafeAreaView>
 		</>
 	);
@@ -68,40 +28,26 @@ const App = () => {
 
 const styles = StyleSheet.create({
 	scrollView: {
-		backgroundColor: Colors.lighter,
+		backgroundColor: "#333333",
+		width: "100%",
+		height: "100%",
 	},
-	engine: {
-		position: "absolute",
-		right: 0,
+	center: {
+		height: "100%",
+		alignItems: "center",
+		justifyContent: "center"
 	},
-	body: {
-		backgroundColor: Colors.white,
+	button: {
+		margin: 30,
+		width: 260,
+		alignItems: "center",
+		backgroundColor: "#710193",
 	},
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: "600",
-		color: Colors.black,
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: "400",
-		color: Colors.dark,
-	},
-	highlight: {
+	buttonText: {
+		fontSize: 20,
 		fontWeight: "700",
-	},
-	footer: {
-		color: Colors.dark,
-		fontSize: 12,
-		fontWeight: "600",
-		padding: 4,
-		paddingRight: 12,
-		textAlign: "right",
+		padding: 20,
+		color: "white",
 	},
 });
 
