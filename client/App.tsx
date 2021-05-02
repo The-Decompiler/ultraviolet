56f1a0d2cef9c945ea26eca5e67f9f67bdb580f0
maxminoS <11131914+maxminoS@users.noreply.github.com>
2021-05-02 21:30:05 +0700

    Create TCP client in the frontend (#3)
    * Initial React-Native TypeScript boilerplate
    
    * Fix bug in JSON parsing
    
    The final value in a JSON object can not end in a comma.
    
    * Add Touchable component to trigger onPress
    
    * Auto-generated package management
    
    * Install and use react-native-tcp-socket
    
    * Create reconnect button for convenience
    
    The Go server had to be restarted every time while the app didn't, so
    this was added and will likely continue to be used for convenient development

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TcpSocket from "react-native-tcp-socket";
 
 import {
 	SafeAreaView,
@@ -9,8 +10,27 @@ import {
 } from "react-native";
 
 const App = () => {
+	const [client, setClient] = useState<TcpSocket.Socket>();
+
+	useEffect(() => {
+		connectSocket();
+		return () => {
+			if (client)
+				client.destroy();
+		};
+	}, []);
+
+	const connectSocket = () => {
+		setClient(TcpSocket.createConnection({
+			port: 27001,
+			host: "localhost",
+		}, () => {}));
+	}
+
 	const handlePress = () => {
-		console.log("Pressed!")
+		if (client)
+			client.write("Pressed!");
+		console.log("Pressed!");
 	}
 
 	return (
@@ -20,6 +40,11 @@ const App = () => {
 					<TouchableHighlight onPress={handlePress} underlayColor="#7A4988" style={styles.button}>
 						<Text style={styles.buttonText}>Tap me!</Text>
 					</TouchableHighlight>
+					<View style={styles.footer}>
+						<TouchableHighlight onPress={connectSocket} underlayColor="#7A4988" style={styles.refresh}>
+							<Text style={styles.refreshText}>Reconnect</Text>
+						</TouchableHighlight>
+					</View>
 				</View>
 			</SafeAreaView>
 		</>
@@ -36,6 +61,22 @@ const styles = StyleSheet.create({
 		height: "100%",
 		alignItems: "center",
 		justifyContent: "center"
+	},
+	footer: {
+		position: "absolute",
+		bottom: 0,
+	},
+	refresh: {
+		width: 105,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#710193",
+		height: 40,
+		borderRadius: 20,
+	},
+	refreshText: {
+		fontSize: 15,
+		color: "white",
 	},
 	button: {
 		margin: 30,
