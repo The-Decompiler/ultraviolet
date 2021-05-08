22a520c3afb4fc6e823a4ac9d00e38ff8f876f75
maxminoS <11131914+maxminoS@users.noreply.github.com>
2021-05-09 01:58:40 +0700

    Remember previously stored address (#20) (#21)
    * Remember previously enterred address
    
    * Install AsyncStorage
    
    * Store address locally

--- client/App.tsx
+++ client/App.tsx
@@ -7,11 +7,13 @@ import { Touchpad } from "./components/Touchpad";
 import { ConnectModal } from "./components/ConnectModal";
 
 import {
+	AsyncStorage,
 	SafeAreaView,
 	StyleSheet
 } from "react-native";
 
-import { connectSocket } from "./utils";
+import { addressToString, connectSocket, convertIpAddress } from "./utils";
+import { DEFAULT_ADDRESS } from "./utils";
 
 export type Address = {
 	port: number,
@@ -19,14 +21,32 @@ export type Address = {
 }
 
 const App = () => {
-	const [address, setAddress] = useState<Address>({ port: 27001, host: "localhost" });
+	const [address, setAddress] = useState<Address>(DEFAULT_ADDRESS);
 	const [showConnectModal, setShowConnectModal] = useState(false);
 	const [connect, setConnect] = useState(false);
 
-	useEffect(() => connectSocket(address), []);
+	useEffect(() => {
+		(async () => {
+			try {
+				const value = await AsyncStorage.getItem("address");
+				if (value !== null)
+					setAddress(convertIpAddress(value));
+			} catch (err) {
+				console.log(err);
+			}
+		})()
+		connectSocket(address)
+	}, []);
 
 	useEffect(() => {
 		if (connect) {
+			(async () => {
+				try {
+					await AsyncStorage.setItem("address", addressToString(address));
+				} catch (err) {
+					console.log(err);
+				}
+			})()
 			connectSocket(address);
 			setConnect(false);
 		}
@@ -37,6 +57,7 @@ const App = () => {
 			<SafeAreaView style={styles.scrollView}>
 				{ showConnectModal &&
 					<ConnectModal
+						address={address}
 						setAddress={setAddress}
 						setConnect={setConnect}
 						setShowConnectModal={setShowConnectModal}
