9ac169c11071a25be1364ef170a7e4cb1b666042
slow typer <slow@slowtyper.com>
2021-05-08 17:14:18 +0300

    Add message parse + action perform
    Types of messages:
    `k<key>` Presses a key
    `t<text>` Types UTF-8 encoded text
    `c<r|m|l>` Clicks right, middle or left mouse button
    `p<r|m|l>` Presses right, middle or left mouse button
    `r<r|m|l>` Releases right, middle or left mouse button
    `m<x> <y>` Moves mouse to relative x,y coords
    s<[-]distance> Scrolls

--- server/main.go
+++ server/main.go
@@ -6,7 +6,11 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
+
+	"github.com/go-vgo/robotgo"
 )
 
 type Connections struct {
@@ -48,11 +52,134 @@ func (conns *Connections) Remove(conn net.Conn) error {
 	return fmt.Errorf("connection '%v' not in connections '%v'", conn, conns)
 }
 
+func getMouseButton(b byte) (string, error) {
+	var button string
+
+	switch b {
+	case 'r':
+		button = "right"
+	case 'm':
+		button = "middle"
+	case 'l':
+		button = "left"
+	default:
+		return "", fmt.Errorf("invalid mouse button '%s'", string(b))
+	}
+
+	return button, nil
+}
+
+func mouseClick(b byte) error {
+	button, err := getMouseButton(b)
+	if err != nil {
+		return err
+	}
+	robotgo.MouseClick(button, false)
+	return nil
+}
+
+func mousePress(b byte) error {
+	button, err := getMouseButton(b)
+	if err != nil {
+		return err
+	}
+	robotgo.MouseToggle("down", button)
+	return nil
+}
+
+func mouseRelease(b byte) error {
+	button, err := getMouseButton(b)
+	if err != nil {
+		return err
+	}
+	robotgo.MouseToggle("up", button)
+	return nil
+}
+
+func mouseMove(msg []byte) error {
+	coords := strings.Split(string(msg), " ")
+
+	if len(coords) > 2 {
+		return fmt.Errorf("invalid mouse move message: %s", string(msg))
+	}
+
+	x, err := strconv.Atoi(coords[0])
+	if err != nil {
+		return fmt.Errorf("failed to parse x coord '%s': %v", coords[0], err)
+	}
+
+	y, err := strconv.Atoi(coords[1])
+	if err != nil {
+		return fmt.Errorf("failed to parse y coord '%s': %v", coords[1], err)
+	}
+
+	robotgo.MoveRelative(x, y)
+
+	return nil
+}
+
+func mouseScroll(msg []byte) error {
+	input := string(msg)
+	distance, err := strconv.Atoi(input)
+	if err != nil {
+		return fmt.Errorf("failed to parse scroll distance '%s': %v", input, err)
+	}
+
+	var direction string
+	if distance < 0 {
+		distance *= -1
+		direction = "up"
+	} else {
+		direction = "down"
+	}
+
+	robotgo.ScrollMouse(distance, direction)
+	return nil
+}
+
+func keyboardKey(msg []byte) error {
+	robotgo.KeyTap(string(msg))
+	return nil
+}
+
+func keyboardText(msg []byte) error {
+	robotgo.TypeStr(string(msg))
+	return nil
+}
+
+func PerformAction(msg []byte) error {
+	if len(msg) < 2 {
+		return fmt.Errorf("invalid message '%s'", string(msg))
+	}
+
+	switch msg[0] {
+	case 'k':
+		return keyboardKey(msg[1:])
+	case 't':
+		return keyboardText(msg[1:])
+	case 'c':
+		return mouseClick(msg[1])
+	case 'p':
+		return mousePress(msg[1])
+	case 'r':
+		return mouseRelease(msg[1])
+	case 'm':
+		return mouseMove(msg[1:])
+	case 's':
+		return mouseScroll(msg[1:])
+	default:
+		return fmt.Errorf("invalid action '%s'", string(msg[0]))
+	}
+}
+
 func (conns *Connections) HandleConnection(conn net.Conn) {
 	defer conns.Remove(conn)
 	defer conn.Close()
 
-	conns.Add(conn)
+	if err := conns.Add(conn); err != nil {
+		log.Println(err)
+		return
+	}
 
 	for {
 		msg := make([]byte, 128)
@@ -63,7 +190,10 @@ func (conns *Connections) HandleConnection(conn net.Conn) {
 		}
 
 		msg = msg[:readLen]
-		fmt.Println(msg)
+
+		if err := PerformAction(msg); err != nil {
+			log.Printf("invalid message '%s': %v\n", string(msg), err)
+		}
 	}
 }
 
