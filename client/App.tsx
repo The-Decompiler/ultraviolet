4d9c6b5b7a9359c4460096ec55e9b6692b2d7bb2
maxminoS <maxminos.1@gmail.com>
2021-05-13 08:04:10 +0700

    Replace mouse button state logic with useReducer and useContext

--- client/App.tsx
+++ client/App.tsx
@@ -6,6 +6,8 @@ import { Reconnect } from "./components/Reconnect";
 import { Touchpad } from "./components/Touchpad";
 import { ConnectModal } from "./components/ConnectModal";
 
+import { LongButtonContextProvider } from "./contexts/LongButtonContext";
+
 import {
 	Dimensions,
 	Image,
@@ -15,8 +17,7 @@ import {
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { addressToString, connectSocket, convertIpAddress, mouseHandler } from "./utils";
-import { MouseButtons, MouseClicks } from "./utils";
+import { addressToString, connectSocket, convertIpAddress } from "./utils";
 import { DEFAULT_ADDRESS } from "./utils";
 
 export type Address = {
@@ -28,10 +29,6 @@ const App = () => {
 	const [address, setAddress] = useState<Address>(DEFAULT_ADDRESS);
 	const [showConnectModal, setShowConnectModal] = useState(false);
 	const [connect, setConnect] = useState(false);
-
-	const [toggleMouseLeft, setToggleMouseLeft] = useState(false);
-	const [toggleMouseMiddle, setToggleMouseMiddle] = useState(false);
-	const [toggleMouseRight, setToggleMouseRight] = useState(false);
 
 	useEffect(() => {
 		(async () => {
@@ -60,29 +57,6 @@ const App = () => {
 		}
 	}, [connect])
 
-	const toggleLongButtonHandler = (button: MouseButtons, turn: boolean | null = null) => {
-		switch (button) {
-			case MouseButtons.LEFT:
-				setToggleMouseLeft((turn != null) ? turn : toggleMouseLeft ? false : true);
-				toggleMouseLeft
-					? mouseHandler(MouseClicks.RELEASE, MouseButtons.LEFT)
-					: mouseHandler(MouseClicks.PRESS, MouseButtons.LEFT);
-				break;
-			case MouseButtons.MIDDLE:
-				setToggleMouseMiddle((turn != null) ? turn : toggleMouseMiddle ? false : true);
-				toggleMouseMiddle
-					? mouseHandler(MouseClicks.RELEASE, MouseButtons.MIDDLE)
-					: mouseHandler(MouseClicks.PRESS, MouseButtons.MIDDLE);
-				break;
-			case MouseButtons.RIGHT:
-				setToggleMouseRight((turn != null) ? turn : toggleMouseRight ? false : true);
-				toggleMouseRight
-					? mouseHandler(MouseClicks.RELEASE, MouseButtons.RIGHT)
-					: mouseHandler(MouseClicks.PRESS, MouseButtons.RIGHT);
-				break;
-		}
-	}
-
 	return (
 		<>
 			<SafeAreaView style={styles.scrollView}>
@@ -98,18 +72,10 @@ const App = () => {
 						setShowConnectModal={setShowConnectModal}
 					/>
 				}
-				<Touchpad
-					toggleLongButtonHandler={toggleLongButtonHandler}
-					toggleMouseLeft={toggleMouseLeft}
-					toggleMouseMiddle={toggleMouseMiddle}
-					toggleMouseRight={toggleMouseRight}
-				/>
-				<MouseClick
-					toggleLongButtonHandler={toggleLongButtonHandler}
-					toggleMouseLeft={toggleMouseLeft}
-					toggleMouseMiddle={toggleMouseMiddle}
-					toggleMouseRight={toggleMouseRight}
-				/>
+				<LongButtonContextProvider>
+					<Touchpad />
+					<MouseClick />
+				</LongButtonContextProvider>
 				<Reconnect
 					setConnect={setConnect}
 					setShowConnectModal={setShowConnectModal}
--- client/components/Touchpad.tsx
+++ client/components/Touchpad.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import { GestureResponderEvent,
 				 StyleSheet,
 				 View
 } from "react-native";
 
+import { LongButtonStateContext, LongButtonDispatchContext } from "../contexts/LongButtonContext";
 import { mouseMove, mouseScroll, mouseHandler } from "../utils";
 import { Position, ScrollPosition, MouseButtons, MouseClicks } from "../utils";
 
@@ -13,20 +14,16 @@ enum Tap { One = MouseButtons.LEFT, Two = MouseButtons.RIGHT, Three = MouseButto
 
 const TAP_INTERVAL = 250;
 
-type Props = {
-	toggleLongButtonHandler: (button: MouseButtons, turn?: boolean | null) => void,
-	toggleMouseLeft: boolean,
-	toggleMouseMiddle: boolean,
-	toggleMouseRight: boolean,
-}
-
-export const Touchpad = ({ toggleLongButtonHandler, toggleMouseLeft, toggleMouseMiddle, toggleMouseRight }: Props) => {
+export const Touchpad = () => {
 	const [prevPosition, setPrevPosition] = useState<Position | null>(null);
 	const [position, setPosition] = useState<Position | null>(null);
 	const [prevScroll, setPrevScroll] = useState<ScrollPosition | null>(null);
 	const [scroll, setScroll] = useState<ScrollPosition | null>(null);
 	const [tap, setTap] = useState<Tap | null>(null);
 	const [time, setTime] = useState(Date.now().valueOf());
+
+	const { toggleMouseLeft, toggleMouseMiddle, toggleMouseRight } = useContext(LongButtonStateContext);
+	const longButtonDispatch = useContext(LongButtonDispatchContext);
 
 	const isNumFingers = (finger: number, event: GestureResponderEvent) => event.nativeEvent.touches.length == finger;
 
@@ -70,15 +67,15 @@ export const Touchpad = ({ toggleLongButtonHandler, toggleMouseLeft, toggleMouse
 				if (tap)
 					switch ((tap as unknown) as MouseButtons) {
 						case MouseButtons.LEFT:
-							if (toggleMouseLeft) toggleLongButtonHandler(MouseButtons.LEFT)
+							if (toggleMouseLeft) longButtonDispatch({ button: MouseButtons.LEFT })
 							else mouseHandler(MouseClicks.CLICK, MouseButtons.LEFT);
 							break;
 						case MouseButtons.MIDDLE:
-							if (toggleMouseMiddle) toggleLongButtonHandler(MouseButtons.MIDDLE)
+							if (toggleMouseMiddle) longButtonDispatch({ button: MouseButtons.MIDDLE })
 							else mouseHandler(MouseClicks.CLICK, MouseButtons.MIDDLE);
 							break;
 						case MouseButtons.RIGHT:
-							if (toggleMouseRight) toggleLongButtonHandler(MouseButtons.RIGHT)
+							if (toggleMouseRight) longButtonDispatch({ button: MouseButtons.RIGHT })
 							else mouseHandler(MouseClicks.CLICK, MouseButtons.RIGHT);
 							break;
 					}
